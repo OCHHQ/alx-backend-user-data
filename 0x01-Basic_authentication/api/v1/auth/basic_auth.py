eaf52e42fdc27314463e16b6567fde2ab08ba80c
OCHHQ <enosejecollins@gmail.com>
2024-11-14 04:30:50 +0100

    update with basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module for API.
+"""
+from api.v1.auth.auth import Auth
+
+class BasicAuth9(Auth):
+    """
+    Basic authentication class that inherit from Auth.
+    """
+    pass
