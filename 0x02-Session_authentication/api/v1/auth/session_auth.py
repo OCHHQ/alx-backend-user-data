eb4c9dfbcde2485cb58f5d82a640a48361f1301f
OCHHQ <enosejecollins@gmail.com>
2024-11-16 07:16:57 +0100

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Session Authentication module. """
+
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """ SessionAuth class that inherits from Auth.
+    This class is empty for now and will be expanded later.
+    """
+    pass
