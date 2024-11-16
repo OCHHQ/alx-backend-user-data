b6f8206f448f6f15249246fa9cc5c71b721cb7ee
OCHHQ <enosejecollins@gmail.com>
2024-11-16 12:02:26 +0100

    update the session_auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,6 +25,22 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
 
+        # Generate a unique Session ID
         session_id = str(uuid4())
+
+        # Store the mapping of session_id to user_id
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a User ID base on a Session ID
+        Args:
+            session with Id: string rep of the session Id
+        Returns:
+            None if session0id is None or not a string
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_for_session_id.get(session_id)
