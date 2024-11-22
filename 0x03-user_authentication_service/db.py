8cfbff4e90cdd0ec7d694b20d5f2d43ca71eadab
OCHHQ <enosejecollins@gmail.com>
2024-11-22 14:25:29 +0100

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,6 +3,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 import bcrypt
 
@@ -12,7 +14,8 @@ class DB:
 
     def __init__(self) -> None:
         """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        # Optional: Comment out these lines to retain data across runs
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -29,7 +32,7 @@ class DB:
         """
         Add a new user to the database
 
-        Args:main.p
+        Args:
             email (str): User's email
             hashed_password (str): Hashed password for the user
 
@@ -42,26 +45,12 @@ class DB:
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """
-        Find a user by arbitrary keyword arguments.
-
-        Args:
-            kwargs: Arbitrary keyword arguments to filter users.
-
-        Returns:
-            User: The first user matching the filter.
-
-        Raises:
-            NoResultFound: If no user matches the filter.
-            InvalidRequestError: If query arguments are invalid.
-        """
+        """Find a user by arbitrary keyword arguments."""
         if not kwargs:
-            raise InvalidRequestError("No query arguments provided")
-
-        query = self._session.query(User).filter_by(**kwargs)
-        user = query.first()
-
-        if user is None:
+            raise InvalidRequestError("No filter arguments provided")
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
             raise NoResultFound("No user found with the given filter")
-
-        return user
+        except Exception as e:
+            raise InvalidRequestError(f"Invalid arguments provided: {e}")
