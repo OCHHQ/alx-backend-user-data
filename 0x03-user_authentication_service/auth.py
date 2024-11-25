4603c40391a2d28fb0a1319f37dfd7b461b9f306
OCHHQ <enosejecollins@gmail.com>
2024-11-25 02:28:42 +0100

    update register feature when new user is add and if user exist raised and exception

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,6 +8,29 @@ from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password with a random salt using bcrypt.
+
+    Args:
+        password (str): The plaintext password to be hashed.
+
+    Returns:
+            bytes: The salted hash of the password.
+    """
+    if not isinstance(password, str):
+        raise TypeError("Password must be a string")
+
+    # Step 1: Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Step 2: Hash the password with the salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    # Step 3: Return the hashed password
+    return hashed_password
+
+
 class Auth:
     """A  Class to interact with the authentication database"""
 
@@ -44,25 +67,3 @@ class Auth:
 
         # litrally return User Object
         return new_user
-
-    def _hash_password(password: str) -> bytes:
-        """
-        Hashes a password with a random salt using bcrypt.
-
-        Args:
-            password (str): The plaintext password to be hashed.
-
-        Returns:
-            bytes: The salted hash of the password.
-        """
-        if not isinstance(password, str):
-            raise TypeError("Password must be a string")
-
-        # Step 1: Generate a salt
-        salt = bcrypt.gensalt()
-
-        # Step 2: Hash the password with the salt
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-
-        # Step 3: Return the hashed password
-        return hashed_password
