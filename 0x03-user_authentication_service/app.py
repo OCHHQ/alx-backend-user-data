e2f4596fe91e053aeeafdbb5fb6c3e29b50bbe34
OCHHQ <enosejecollins@gmail.com>
2024-11-25 03:04:40 +0100

    set up a flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Baic flask app module for user authentication
+This module create a simple web application using
+Flask framework that serves as a foundation for
+our authentication service
+"""
+from flask import Flask, jsonify
+
+app = flask(__name__)
+
+@app.route('/', methods=['GET'])
+def welcome() -> jsonify:
+    """
+    Root endpoint welcome users to the authentication service
+
+    Route:
+        -URL:/
+        _Method: GET
+
+   Return:
+    Dict[str, str]: JSON response containing welcome
+    format: {"message": "Bienvenue"}
+   
+   Response Details:
+    -Content-Type: application/json
+    -Status Code: 200 (ok)
+    -Character Encoding: UTF-8
+    """
+    return jsonify({"messasge": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
