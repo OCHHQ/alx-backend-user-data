6d971eea928ec54523ef5115daf4fbd4b0c79bcd
OCHHQ <enosejecollins@gmail.com>
2024-11-27 01:16:22 +0100

    update auth.py class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,3 +67,24 @@ class Auth:
 
         # litrally return User Object
         return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validate a user's login credentials.
+
+        Args:
+            email (str): User's email.
+            password (str): User's password.
+
+        Returns:
+            bool: True if credentials are valid, False otherwise.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user and bcrypt.checkpw(
+                    password.encode('utf-8'),
+                    user.hashed_password):
+                return True
+        except Exception:
+            pass
+        return False
